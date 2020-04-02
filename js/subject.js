f2b88946d8831f950e432b61e67d22e96187c6da
Alexandra <a.r.chumachenko@gmail.com>
2020-04-02 12:40:17 +0300

    Remove warnings

--- js/subject.js
+++ js/subject.js
@@ -5,26 +5,28 @@
 
 let data_names = [{id: '1', last_name: "Surname", first_name: "Name", second_name: "Pobatkovi"},
     {id: '2', last_name: "Surname", first_name: "Name1", second_name: "Pobatkovi"},
-    {id: '3', last_name: "Surname", first_name: "Name", second_name: "Pobatkovi"},]
+    {id: '3', last_name: "Surname", first_name: "Name", second_name: "Pobatkovi"},];
 
 let theme_names = [{id: '112314',name:"Name of the first theme" , number:'1', hours:'10', coef_special: 0.3, coef_diary: 0.01, coef_theme: 0.5},
     {id: '112214', name:"Name of the second theme" ,number:'2', hours:'11', coef_special: 0.3, coef_diary: 0.01, coef_theme: 0.5},
-    {id: '112514', name:"Name of the third theme" ,number:'3', hours:'6', coef_special: 0.3, coef_diary: 0.01, coef_theme: 0.5}]
+    {id: '112514', name:"Name of the third theme" ,number:'3', hours:'6', coef_special: 0.3, coef_diary: 0.01, coef_theme: 0.5}];
 
 let lesson_names = [{id: '242145', theme: 'Theme of lesson 1', date: new Date(2020, 4, 5),hometask: 'write esse'},
     {id: '24234',theme: 'Theme of lesson 2',date: new Date(2020, 4, 6),},
-    {id: '24234',theme: 'Theme of lesson 3',date: new Date(2020, 4, 7),}]
+    {id: '24234',theme: 'Theme of lesson 3',date: new Date(2020, 4, 7),}];
 
 let dairy_data_marks = [{date: new Date(2019, 3, 23), marks:[]},
     {date: new Date(2019, 4, 23), marks:[{id: '1', value: '10', visible:'true'}, {id:'2', value:'9', visible:'true'}]},
     {date: new Date(2019, 3, 24), marks:[]},
     {date: new Date(2019, 3, 25), marks:[{id:'3', value:'7', visible:'true'}]},
-    {date: new Date(2019, 4, 26), marks:[{id: '1', value: '12', visible:'true'}]}, ]
+    {date: new Date(2019, 4, 26), marks:[{id: '1', value: '12', visible:'true'}]}, ];
 let special_data_marks = [{name: "To be or not to be", type: "poem", marks: [{id:'1', visible:'false', value:5, comment:"once more chance"}, {id:'2',value: 10, visible:'true' }]},
     {name: "Test12", type: "test", marks: [{id:'1', value:'12', visible:'true'}, {id:'3', value:'6', visible: "true"}]}];
+
 let them_marks = [{id: '1', theme: 'theme1', value: '10', visible:'false'},
     {id: '2', theme: 'theme1', value: '8', visible:'true'},
     {id: '3', theme: 'theme1', value: '9', visible:'true'},];
+
 let attend = [
     {date: new Date(2019, 4, 23), n_attend_id : []},
     {date: new Date(2019, 4, 24), n_attend_id : [1]},
@@ -88,7 +90,7 @@ $(document).on('click', '#add_theme', function () {
 });
 $(document).on('click', '#add_lesson', function () {
     if(!createLesson) {
-        createLessonDivAdding()
+        createLessonDivAdding();
         createLesson=true;
         return;
     }
@@ -96,7 +98,7 @@ $(document).on('click', '#add_lesson', function () {
     createLesson = false;
 });
 $(document).on('click', '.theme_list', function (e) {
-    if(e.target.type == "submit")return;
+    if(e.target.type === "submit")return;
     //TODO view Theme
     let id= $(this).data('id');
     createViewLessonsFromThemeById(id);
@@ -118,7 +120,7 @@ $(document).on('click', '.delete_lesson', function () {
 });
 $(document).on('click', '#back, #cancel', function(){
     $("#bacground_adding_parents").remove()
-})
+});
 
 /************************Function***************************/
 /*-----Left-menu navigating-----*/
@@ -140,26 +142,26 @@ function removing(item, div) {
 }
 /*------------------------------------------*/
 function  createLessonDiv(id){
+    let content =   $('#content');
     let theme = $(`<div class="cotainer lesson">`);
     theme.append($(`<button class="btn my_btn btn-outline-success" id="add_lesson">`).text('Створити урок'));
     theme.append($(`<div id="add_lesson_form">`));
-    $('#content').append(theme);
+    content.append(theme);
     let them_view = $(`<div id="lesson_view">`);
 
     //todo ajax lessons by theme id
     lesson_names.forEach(l=>them_view.append(lessons(l)));
-    $('#content').append(them_view);
+    content.append(them_view);
 }
 function  createThemeDiv(){
+    let content =   $('#content');
     let theme = $(`<div class="cotainer theme">`);
     theme.append($(`<button class="btn my_btn btn-outline-success" id="add_theme">`).text('Додати тему'));
     theme.append($(`<div id="add_theme_form">`));
-    $('#content').append(theme);
+    content.append(theme);
     let them_view = $(`<div id="theme_view">`);
-    theme_names.forEach(the=> them_view.append(themes(the)))
-
-
-    $('#content').append(them_view);
+    theme_names.forEach(the=> them_view.append(themes(the)));
+    content.append(them_view);
 
 }
 
@@ -167,7 +169,7 @@ function createLessonDivAdding() {
     let form = $('<form class="container" method="post" id="form_l">');
 
     let input_theme = create_input_group('text', 'Тема уроку', "", "lesson-theme");
-    let input_num = create_input_group('date', 'Дата проведення', "", "date")
+    let input_num = create_input_group('date', 'Дата проведення', "", "date");
     let input_hometask =  create_input_group('text', 'Домашнє завдання', "", "lesson-theme");
     form.append(input_theme);
     form.append(input_num);
@@ -178,12 +180,12 @@ function createLessonDivAdding() {
     $('#add_lesson_form').append(form);
 }
 function createThemeDivAdding(){
-let form = $('<form class="container" method="post" id="form">')
+let form = $('<form class="container" method="post" id="form">');
     let input_name = create_input_group('text', 'Назва', "", "theme");
     let input_num =  create_input_group('number', 'Порядковий номер', "", "theme", 1);
-    let coef_sp = create_input_group('number', 'Коефіцієнт спеціальних оцінок', "", "coef-special", 0, "", 0.01)
-    let coef_d =   create_input_group('number', 'Коефіцієнт поточних оцінок', "", "coef-diary", 0, "", 0.01)
-    let coef_th =   create_input_group('number', 'Коефіцієнт тематичної оцінок', "", "coef-theme", 0, "", 0.01)
+    let coef_sp = create_input_group('number', 'Коефіцієнт спеціальних оцінок', "", "coef-special", 0, "", 0.01);
+    let coef_d =   create_input_group('number', 'Коефіцієнт поточних оцінок', "", "coef-diary", 0, "", 0.01);
+    let coef_th =   create_input_group('number', 'Коефіцієнт тематичної оцінок', "", "coef-theme", 0, "", 0.01);
     form.append(input_name);
     form.append(input_num);
     form.append(coef_sp);
@@ -200,12 +202,12 @@ data-coef_special="${cs}" data-coefdiary="${cd}", data-coef_theme="${ct}" class=
   */
 function createEditionThemeView(a) {
     let back = $(` <button id="back" type="reset" class="btn my_btn btn-outline-success" >`).text("Назад");
-    let form = $('<form class="container" method="post" id="form_them_edit">')
+    let form = $('<form class="container" method="post" id="form_them_edit">');
     let input_name = create_input_group('text', 'Назва', a.data('name'), "theme");
     let input_num =  create_input_group('number', 'Порядковий номер', a.data('number'), "theme", 1);
-    let coef_sp = create_input_group('number', 'Коефіцієнт спеціальних оцінок',  a.data('coef_special'), "coef-special", 0, "", 0.01)
-    let coef_d =   create_input_group('number', 'Коефіцієнт поточних оцінок', a.data('coefdiary'), "coef-diary", 0, "", 0.01)
-    let coef_th =   create_input_group('number', 'Коефіцієнт тематичної оцінок', a.data('coef_theme'), "coef-theme", 0, "", 0.01)
+    let coef_sp = create_input_group('number', 'Коефіцієнт спеціальних оцінок',  a.data('coef_special'), "coef-special", 0, "", 0.01);
+    let coef_d =   create_input_group('number', 'Коефіцієнт поточних оцінок', a.data('coefdiary'), "coef-diary", 0, "", 0.01);
+    let coef_th =   create_input_group('number', 'Коефіцієнт тематичної оцінок', a.data('coef_theme'), "coef-theme", 0, "", 0.01);
     form.append(back).append(input_name);
     form.append(input_num);
     form.append(coef_sp);
@@ -219,16 +221,16 @@ function createEditionThemeView(a) {
 function createEditionLessonView(a) {
     let back = $(` <button id="back" class="btn my_btn btn-outline-success" >`).text("Назад");
 
-    let form = $('<form class="container" method="post" id="form_them_edit">')
+    let form = $('<form class="container" method="post" id="form_them_edit">');
     let input_name = create_input_group('text', 'Тема: ', a.data('theme'), "theme");
     let input_num =  create_input_group('date', 'Дата проведення:', a.data('date'), "date");
-    let coef_sp = create_input_group('text', 'Домашнє завдання: ',  (a.data('hometask')=='undefined')?"":a.data('hometask'), "hometask")
-     form.append(back).append(input_name);
+    let coef_sp = create_input_group('text', 'Домашнє завдання: ',  (a.data('hometask')==='undefined')?"":a.data('hometask'), "hometask");
+    form.append(back).append(input_name);
     form.append(input_num);
     form.append(coef_sp);
     let submit = $(`<input type="submit" class="input-group-text">`);
-   form.append(submit)
-    createWindow(form)
+    form.append(submit);
+    createWindow(form);
 }
 function createViewLessonsFromThemeById(id){
 //todo ajax to get lessons
@@ -249,7 +251,7 @@ $(document).on('click', '#add_attend_column', function(){
       btn_div.append(button_ok).append(button_cancel);
       div.append(input_date).append(btn_div);
       createWindow(div);
-})
+});
 $(document).on('click', '#ok', function(){
     let date =  document.getElementsByName('date_creating')[0].valueAsDate;
     //send to server that there values of attend-dates
@@ -258,9 +260,10 @@ $(document).on('click', '#ok', function(){
 }
 );
 function createNewAttendColumn(date){
-    let start =$( $('#table_caption').children()[$('#table_caption').children().length-1]).data('column')+1;
+    let table_caption = $('#table_caption');
+    let start =$(table_caption.children()[table_caption.children().length-1]).data('column')+1;
     let t = $(`<td data-column="${start}" data-date="${cutData(date)}" data-type="attend">`).text(cellDate(date));
-    $('#table_caption').append(t);
+    table_caption.append(t);
     data_names.forEach(st => {
         let row = $("#table-row-"+st.id);
         let td = $(`<td data-column="${start}" data-date="${cutData(date)}" data-type="attend">`);
@@ -272,7 +275,7 @@ $(document).on('click', '#create_attend_by_period', function () {
     table.empty();
     let capt = $(`<tr id="table_caption">`);
     let tr = $(`<td>`).text('Прізвище');
-    table.append(capt.append(tr))
+    table.append(capt.append(tr));
 
     data_names.forEach(student=> {
         let td = $(`<tr id="table-row-${student.id}">`);
@@ -286,13 +289,13 @@ $(document).on('click', '#create_attend_by_period', function () {
         console.log('here');
         let mark_cell = attend[i];
         console.log(mark_cell);
-        let date_format = cellDate(mark_cell.date)
+        let date_format = cellDate(mark_cell.date);
         console.log(date_format);
         let date_cell = $(`<td data-column="${i}" data-date=${cutData(mark_cell.date)} data-type="attend">`).text(date_format);
         capt.append(date_cell);
         for(let j = 0; j<data_names.length; j++ ){
             let id = data_names[j].id;
-            let mark= mark_cell.n_attend_id.find(el=> el==id);
+            let mark= mark_cell.n_attend_id.find(el=> el===id);
             let row = $("#table-row-"+id);
             if(mark===undefined) {
                 let td = $(`<td data-column="${i}" data-date=${cutData(mark_cell.date)} data-type="attend">`);
@@ -305,14 +308,14 @@ data-mark-visible="${mark.visible}">`).text('H');
         }
     }
     $('#attend_view_table').removeClass('hidden');
-})
+});
 $(document).on('click', 'td[data-type="attend"]', function(){
 
-    if($(this).parent().attr('id') == "table_caption"){
+    if($(this).parent().attr('id') === "table_caption"){
         //todo show asking delete
         return;
     }
-   if( $(this).text() !='H') {
+   if( $(this).text() !=='H') {
        //todo send to server date id and subject
        // wait for answer and add id of cell to id
        $(this).text('H');
@@ -322,7 +325,7 @@ $(document).on('click', 'td[data-type="attend"]', function(){
        $(this).text('');
    }
 
-} )
+} );
 
 $(document).on('click', '#create_marks_by_period', function(){
     console.log('here');
@@ -346,12 +349,12 @@ $(document).on('click', '#create_marks_by_period', function(){
             if(dairy_data_marks[i].marks === undefined || dairy_data_marks[i].marks.length===0)
                 continue;
             let mark_cell = dairy_data_marks[i];
-            let date_format = cellDate(mark_cell.date)
+            let date_format = cellDate(mark_cell.date);
             let date_cell = $(`<td data-column="${i}" data-date=${mark_cell.date} data-type="dairy">`).text(date_format);
             caption.append(date_cell);
             for(let j = 0; j<data_names.length; j++ ){
                  let id = data_names[j].id;
-                 let mark= mark_cell.marks.find(el=> el.id==id);
+                 let mark= mark_cell.marks.find(el=> el.id===id);
                  let row = $("#table-row-"+id);
                  if(mark===undefined) {
                      let td = $(`<td data-column="${i}" data-date=${mark_cell.date} data-type="dairy">`);
@@ -379,7 +382,7 @@ data-mark-visible="${mark.visible}">`).text(mark.value);
         for(let j = 0; j<data_names.length; j++ ){
             let id = data_names[j].id;
 
-            let mark= mark_cell.marks.find(el=> el.id==id);
+            let mark= mark_cell.marks.find(el=> el.id===id);
             let row = $("#table-row-"+id);
             console.log(id +" "+ mark);
 
@@ -392,7 +395,7 @@ data-mark-visible="${mark.visible}">`).text(mark.value);
                 console.log(td);
                 continue;
             }
-            let td = $(`<td data-column="${column}"data-type="special" data-mark-work-type = ${mark_cell.type}
+            let td = $(`<td data-column="${column}" data-type="special" data-mark-work-type = ${mark_cell.type}
                             data-mark-name="${mark_cell.name}" data-mark-value="${mark.value}"
 data-mark-visible="${mark.visible}">`).text(mark.value);
             row.append(td);
@@ -433,7 +436,7 @@ function createPeriodForm() {
     let submit = $(`<input type="submit" id="create_attend_by_period" class="input-group-text">`);
     div.append(dataform.append(inputdate1).append(inputdate2).append(submit));
     let marks_view = $(`<div id="attend_view_table" class="hidden">`);
-    let button = $(`<button class="btn-outline-dark btn" id="add_attend_column">`).text("Додати стовпчик")
+    let button = $(`<button class="btn-outline-dark btn" id="add_attend_column">`).text("Додати стовпчик");
     let table = $(`<table id='attend' >`);
     marks_view.append(button).append(table);
     $('#content').append(div).append(marks_view);
@@ -443,17 +446,16 @@ function createPeriodForm() {
 function createMarksView(){
     let div = $(`<div id="choose_period">`);
     let dataform = $(`<div class="form">`); //можливо краще форму?
-    let inputdate1 = create_input_group('date', 'Дата початку', '', 'start_date');
-    let inputdate2 = create_input_group('date', 'Кінцева дата', '', 'end_date');
+    let inputdate1 = create_selected_input(['theme1', 'theme2', 'theme3'], 'Tема:', '', 'Оберіть тему', 'theme');
 
     let submit = $(`<input type="submit" id="create_marks_by_period" class="input-group-text">`);
-    div.append(dataform.append(inputdate1).append(inputdate2).append(submit));
+    div.append(dataform.append(inputdate1).append(submit));
     let marks_view = $(`<div id="marks_view_table">`);
     let div_table_wrapper=$(`<div id="marks_table_wrapper">`);
     let table = $(`<table id='marks'>`);
     let caption = $(`<tr id="table_caption">`);
     let tr = $(`<td>`).text('Прізвище');
-    table.append(caption.append(tr))
+    table.append(caption.append(tr));
     marks_view.append(div_table_wrapper.append(table));
     $('#content').append(div).append(marks_view);
 }
@@ -533,8 +535,8 @@ let lessons = ({
     div.append(header);
     let information = $(` <div class="information">`);
     let div_data = $(` <div>`).text('Дата проведення: '+date.toLocaleDateString());
-    information.append(div_data)
-    if(ht !== undefined && ht.length!=0) {
+    information.append(div_data);
+    if(ht !== undefined && ht.length!==0) {
         let div_hours = $(` <div>`).text('Домашнє завдання: ' + ht);
         information.append(div_hours);
     }
@@ -542,7 +544,7 @@ let lessons = ({
     div.append(information);
     return div;
 
-}
+};
 let themes = ({id: id,
                name: name,
                hours: hours,
@@ -569,10 +571,7 @@ data-coef_special="${cs}" data-coefdiary="${cd}", data-coef_theme="${ct}" class=
 
     div.append(information);
     return div;
-
-
-
-}
+};
 
 
 /*------To do on update--------*/
